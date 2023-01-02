695acd1e34a3c4743c27f00554185be11f2d9c55
Orhan-Kahraman <herorhan999@gmail.com>
2023-01-02 19:05:21 +0300

    Happy New Year

--- lib/Home/template_elements/one_answer.dart
+++ lib/Home/template_elements/one_answer.dart
@@ -1,10 +1,18 @@
+import 'package:fair_app/Home/homespecialWidgets/custom_dialog.dart';
 import 'package:fair_app/shared/const.dart';
+import 'package:fair_app/shared/helpers.widget.dart';
 import 'package:flutter/material.dart';
 
-class OneAnswer extends StatelessWidget {
-  OneAnswer({super.key});
-  final TextEditingController controller = TextEditingController();
+class OneAnswer extends StatefulWidget {
+  const OneAnswer({super.key});
 
+  @override
+  State<OneAnswer> createState() => _OneAnswerState();
+}
+
+class _OneAnswerState extends State<OneAnswer> {
+  final TextEditingController controller = TextEditingController();
+  List<Widget> options = [];
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -15,7 +23,7 @@ class OneAnswer extends StatelessWidget {
           color: kcCuriousBlue,
           borderRadius: BorderRadius.circular(20)),
       height: height * 0.2,
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
       child: Column(
         children: [
           Padding(
@@ -23,24 +31,66 @@ class OneAnswer extends StatelessWidget {
             child: TextField(
               controller: controller,
               decoration: const InputDecoration(hintText: "Sorunuz?"),
+              minLines: 1,
+              maxLines: 2,
             ),
           ),
-          Row(
+          const SizedBox(height: 5),
+          Column(
+            mainAxisSize: MainAxisSize.max,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 40),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: kcwhite54,
-                      foregroundColor: kcCuriousBlue),
-                  child: const Text("+ Add Option"),
-                ),
+              ChoiceWidget(
+                  width: width,
+                  height: height,
+                  choiceText:
+                      "dfsdlşkgsdfl sdlşfkdlşklşf kdslşf klşdsfgl kdflşgk"),
+              ElevatedButton(
+                onPressed: () =>
+                    showDialog(context: context, builder: mycustomDialog),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: kcwhite54, foregroundColor: kcCuriousBlue),
+                child: const Text("+ Add Option"),
               ),
             ],
           )
         ],
       ),
+    );
+  }
+}
+
+class ChoiceWidget extends StatelessWidget {
+  const ChoiceWidget({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.choiceText,
+  }) : super(key: key);
+
+  final double width;
+  final double height;
+  final String choiceText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      alignment: Alignment.centerLeft,
+      width: width * 0.8,
+      height: height * 0.05,
+      decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: Colors.amber,
+          borderRadius: BorderRadius.circular(20)),
+      child: InkWell(
+          onTap: () {
+            print("object");
+          },
+          highlightColor: Colors.amber,
+          child: Text(
+            choiceText,
+            style: Theme.of(context).textTheme.bodySmall,
+          )),
     );
   }
 }
